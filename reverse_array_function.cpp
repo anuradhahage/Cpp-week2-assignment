d41c03b7797459eb0bfe4f877b1b1c7c07545417
Anuradha Hage <vhage656@gmail.com>
2026-09-12 10:27:16 +0530

    Implement function to reverse an array

---
+++ reverse_array_function.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int n) {
+    int start = 0;
+    int end = n - 1;
+
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    reverseArray(arr, n);
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
